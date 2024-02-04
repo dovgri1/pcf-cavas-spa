1498b26be3affec81acdbd4b9ea7529ccff6c915
dovgri1 <dovydas.grigaitis@gmail.com>
2024-02-04 13:36:34 +0200

    Cleans and formats the document.

--- canvasspaapp/components/Application.tsx
+++ canvasspaapp/components/Application.tsx
@@ -1,71 +1,70 @@
-import { AppBar, Box, CssBaseline, Drawer, Toolbar, dividerClasses } from '@mui/material'
-import * as React from 'react'
-import { ApplicationTopBar } from './ApplicationTopBar';
-import {ApplicationBottomNavigation} from './ApplicationBottomNavigation'
-import {Theme} from './Theme';
-import { ThemeProvider } from '@mui/material/styles';
-import { GridComponent } from './GridComponent';
-
+import { Box } from "@mui/material";
+import * as React from "react";
+import { ApplicationTopBar } from "./ApplicationTopBar";
+import { ApplicationBottomNavigation } from "./ApplicationBottomNavigation";
+import { Theme } from "./Theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { GridComponent } from "./GridComponent";
 
 export const Application: React.FC = () => {
-    const [childData, setChildData] = React.useState("");
+  const [childData, setChildData] = React.useState("");
 
-    const handleBottomNavigationChange = (data: string) => {
-        setChildData(data);
-        console.log(data)
-      };
-
-    const ComponentToReturn = () => {
-        if(childData == 'Selected index is 1'){
-            return (
-                <GridComponent></GridComponent>
-            )
-        }else{
-            return (
-                <GridComponent></GridComponent>
-            )
-        }
-
-    }
-
-    return (
-        <ThemeProvider theme={Theme}>
-            <Box sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: '85vh',
-                    width: '100%',
-                    justifyContent:'space-between'
-                    }}>
-                        <Box sx={{ 
-                                    justifyContent:'start', 
-                                    height:'15%'
-                                }}>
-                            <ApplicationTopBar />
-                        </Box>
-                        <Box sx={{
-                                    justifyContent:'center', 
-                                    height:'75%',
-                                    display:'flex',
-                                    flexDirection: 'row'
-                                }}>
-                                    
-                            <ComponentToReturn></ComponentToReturn>
-                        </Box>
-                        <Box sx={{ 
-                                    position: 'relative', 
-                                    width: '100%', 
-                                    height:'10%',
-                                    bottom: 0, 
-                                    left: 0, 
-                                    zIndex: 1000,
-                                    justifyItems:'end'
-                                }}>
-                            <ApplicationBottomNavigation onBottomNavigationChange={handleBottomNavigationChange} />
-                        </Box>
-                </Box>
-        </ThemeProvider>
-      
-    );
+  const handleBottomNavigationChange = (data: string) => {
+    setChildData(data);
   };
 
+  const ComponentToReturn = () => {
+    if (childData == "Selected index is 1") {
+      return <GridComponent></GridComponent>;
+    } else {
+      return <GridComponent></GridComponent>;
+    }
+  };
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "85vh",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            justifyContent: "start",
+            height: "15%",
+          }}
+        >
+          <ApplicationTopBar />
+        </Box>
+        <Box
+          sx={{
+            justifyContent: "center",
+            height: "75%",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <ComponentToReturn></ComponentToReturn>
+        </Box>
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            height: "10%",
+            bottom: 0,
+            left: 0,
+            justifyItems: "end",
+          }}
+        >
+          <ApplicationBottomNavigation
+            onBottomNavigationChange={handleBottomNavigationChange}
+          />
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+};
--- canvasspaapp/components/TableComponent.tsx
+++ canvasspaapp/components/TableComponent.tsx
@@ -1,125 +1,145 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box, TableFooter, TablePagination } from '@mui/material';
-import { _context } from '../services/DataService';
-import { Column, IsIndex, RecordItem } from '../interfaces/interfaces';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box, TablePagination } from "@mui/material";
+import { _context } from "../services/DataService";
+import { Column, RecordItem } from "../interfaces/interfaces";
 
+export const TableComponent: React.FC = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  const [items, setItems] = React.useState<RecordItem[]>([]);
+  const [columns, setColumns] = React.useState<Column[]>([]);
+  const dataSet: ComponentFramework.PropertyTypes.DataSet =
+    _context.parameters.sampleDataSet;
 
+  React.useEffect(() => {
+    const newColumns: Column[] = dataSet.columns.map((column: any) => ({
+      key: column.name,
+      name: column.alias,
+      fieldName: column.displayName,
+      minWidth: column.visualSizeFactor,
+      maxWidth: column.visualSizeFactor,
+      isResizable: true,
+      isIndex: column,
+    }));
+    setColumns(newColumns);
 
-export const TableComponent : React.FC = () => {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const handleChangePage = (event: unknown, newPage: number) => {setPage(newPage)};
-    const [items, setItems] = React.useState<RecordItem[]>([]);
-    const [columns, setColumns] = React.useState<Column[]>([]);
-    const dataSet : ComponentFramework.PropertyTypes.DataSet = _context.parameters.sampleDataSet;
-
-    React.useEffect(() => {
-      const newColumns: Column[] = dataSet.columns.map(
-        (column: any) => ({
-            key: column.name,
-            name: column.alias,
-            fieldName: column.displayName,
-            minWidth: column.visualSizeFactor,
-            maxWidth: column.visualSizeFactor,
-            isResizable: true,
-            isIndex: column,
-        })
-    )
-    setColumns(newColumns)
-
-    const newItems : RecordItem[] = dataSet.sortedRecordIds.map(
+    const newItems: RecordItem[] = dataSet.sortedRecordIds.map(
       (recordId: any) => {
-          const record = dataSet.records[recordId]
-          const item: RecordItem = { id: recordId }
-          newColumns.forEach((column) => {
-              item[column.name] = record.getValue(column.key)
-          })
-          return item
+        const record = dataSet.records[recordId];
+        const item: RecordItem = { id: recordId };
+        newColumns.forEach((column) => {
+          item[column.name] = record.getValue(column.key);
+        });
+        return item;
       }
-    )
+    );
     setItems(newItems);
+  }, [_context.parameters.sampleDataSet]);
 
-    },[_context.parameters.sampleDataSet]);
-
-    function RenderCells(props: { columns: Column[]; row: RecordItem }){
-  return (
-    <React.Fragment>
+  function RenderCells(props: { columns: Column[]; row: RecordItem }) {
+    return (
+      <React.Fragment>
         {props.columns.map((column) => {
-            if (column.isIndex.dataType.includes('SingleLine')) {
-                return (
-                    <TableCell key={column.name}>
-                        {props.row[column.name]}
-                    </TableCell>
-                )
-            } else if (column.isIndex.dataType === 'Lookup.Simple') {
-                return (
-                    <TableCell key={column.name}>
-                        {props.row[column.name]?.name}
-                    </TableCell>
-                )
-            } else if (column.isIndex.dataType.includes('OptionSet')) {
-                return <TableCell key={column.name}>{props.row[column.name]}</TableCell>
-            } else {
-                return <TableCell key={column.name}>{props.row[column.name]}</TableCell>
-            }
+          if (column.isIndex.dataType.includes("SingleLine")) {
+            return (
+              <TableCell key={column.name}>{props.row[column.name]}</TableCell>
+            );
+          } else if (column.isIndex.dataType === "Lookup.Simple") {
+            return (
+              <TableCell key={column.name}>
+                {props.row[column.name]?.name}
+              </TableCell>
+            );
+          } else if (column.isIndex.dataType.includes("OptionSet")) {
+            return (
+              <TableCell key={column.name}>{props.row[column.name]}</TableCell>
+            );
+          } else {
+            return (
+              <TableCell key={column.name}>{props.row[column.name]}</TableCell>
+            );
+          }
         })}
-    </React.Fragment>
-  )
-    }
+      </React.Fragment>
+    );
+  }
 
   return (
     <React.Fragment>
-      <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', width:'100%', gap:2}}>
-      <TableContainer elevation={10} component={Paper} sx={{ width:'100%',  height: '100%'}}>
-            <Table sx={{width:'100%' }} size='small' aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                  {
-                    columns.map((column) => (
-                      <TableCell key={column.name}>
-                          {column.fieldName}
-                      </TableCell>
-                    ))
-                  }
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {(rowsPerPage > 0 
-                   ? items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) 
-                   : items ).map((item) => (
-                        <TableRow
-                          hover
-                          onClick= {(event) => console.log(event)}
-                          key={item.id}
-                          sx={{ '&:last-child td, &:last-child th': { border: 0 }, cursor:'pointer' }}
-                          >
-                          <RenderCells columns={columns} row={item}></RenderCells>
-                        </TableRow>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          width: "100%",
+          gap: 2,
+        }}
+      >
+        <TableContainer
+          elevation={10}
+          component={Paper}
+          sx={{ height: "100%" }}
+        >
+          <Table sx={{ width: "100%" }} size="small" aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell key={column.name}>{column.fieldName}</TableCell>
                 ))}
-                </TableBody>
-                
-            </Table>   
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? items.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : items
+              ).map((item) => (
+                <TableRow
+                  hover
+                  onClick={(event) => console.log(event)}
+                  key={item.id}
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                    cursor: "pointer",
+                  }}
+                >
+                  <RenderCells columns={columns} row={item}></RenderCells>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </TableContainer>
-        <Paper elevation={10} sx={{display:'flex', justifyContent:'center', justifyItems:'center', width: '100%', height:'12%'}}>
+        <Paper
+          elevation={10}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            justifyItems: "center",
+            width: "100%",
+            height: "12%",
+          }}
+        >
           <TablePagination
-              component="div"
-              count={items.length}
-              rowsPerPage={10}
-              page={page}
-              onPageChange={handleChangePage}
-            />
+            component="div"
+            count={items.length}
+            rowsPerPage={10}
+            page={page}
+            onPageChange={handleChangePage}
+          />
         </Paper>
       </Box>
-
-        
     </React.Fragment>
-   
   );
-}
+};
